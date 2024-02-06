451c1a0619f925a490977f1b247b88743102df61
Your Name <you@example.com>
2024-02-06 10:55:28 +0000

    remove debug

--- app.py
+++ app.py
@@ -560,5 +560,5 @@ def update_div(clustering_type):
 
 if __name__ == "__main__":
     app.run_server(
-        app.run_server(debug=True, host='0.0.0.0')
+        app.run_server(debug=False, host='0.0.0.0')
     )
